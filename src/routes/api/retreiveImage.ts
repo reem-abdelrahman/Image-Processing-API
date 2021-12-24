45fb622d9389f4865a8d09731f920d2153b4503d
reem <r33mk48@gmail.com>
2021-12-24 02:36:25 +0200

    modified image processor code

--- src/routes/api/retreiveImage.ts
+++ src/routes/api/retreiveImage.ts
@@ -4,21 +4,27 @@ import express, { query } from 'express';
 import {promises as fsPromises} from 'fs';
 import path from 'path';   
 import sharp from 'sharp';
+import app from '../..';
+import imageProcess from './imageProcess';
 const retreiveImage = express.Router();
 const imagePath: string = 'src/images';
 const resizedImagePath: string = 'src/images/resized';
+const absoluteResizedImagePath: string = path.resolve(resizedImagePath);
+const absoluteImagePath: string = path.resolve(imagePath);
+
+
 retreiveImage.use('/', express.static('src/images'));
-retreiveImage.get('/', async (req, res, next) =>
 
-{
 
+retreiveImage.get('/',imageProcess, async (req, res) => {
+console.log(absoluteResizedImagePath)
    let imageName: string = req.query.image as string;
    let heightString: string = (req.query.height) as string ;
    let heightN: number = parseInt(heightString);
    let widthString: string = req.query.width as string;
    let widthN: number = parseInt(widthString);
-   let resizedImage= `${resizedImagePath}/${imageName}W${widthN}H${heightN}.jpg`
-   let originalImage = `${imagePath}/${imageName}.jpg`
+   let resizedImage: string = `${resizedImagePath}/${imageName}W${widthN}H${heightN}.jpg`
+   let originalImage: string = `${imagePath}/${imageName}.jpg`
    
 
 
@@ -38,33 +44,58 @@ retreiveImage.get('/', async (req, res, next) =>
    
 // Resize the Image
 
- (async function resizeImage(): Promise<void> {	
+ /*(async function resizeImage(): Promise<void> {	
    try{	
-   
+            
             await sharp(`${originalImage}`)
             .resize(Number(heightN), Number(widthN))
-            
-            .toFile(resizedImage)         
+           .toFile(resizedImage) 
+           
+                   
          } catch (error) { 
                      console.log(error + ' error with sharp');
-                     
-                  
+         }
+   })();  */
+         const imageProcess= sharp(`${originalImage}`) 
+            .resize(Number(heightN), Number(widthN))
+            .toFile(resizedImage, (err:Error)=>{
+               if(err) {res.status(400).send(err) }
+               else {
+                  const relative:string = resizedImage
+                  const absolute:string = path.resolve(relative)
+                  console.log(absolute)
+               //   res.readFile(absolute, (err: Error)=> {
+                //     res.status(200).send('well done. Resized image loaded successfuly')
+               //   })
                }
-   })();         
-           
-       function readImage(outtputFile: any) {
+            })
+        // res.readFile(resizedImage, (err: Error)=> {
+          //  res.status(400).send(err)
+        // })
+        // .then(() =>{
+        //    res.readFile(resizedImage)
+       //  })
+         
+
+        // const absolImg = path.resolve(`/home/reem/Image-Processing-API/src/images/resized/$${imageName}W${widthN}H${heightN}.jpg}`)
+       //  res.readFile(absolImg) 
+
+     /*  function readImage(outtputFile: any) {
          try {
             const readFile = fsPromises.readFile(resizedImage);
          } catch (error) {
             console.log(error + ':readImage()');
          }  
    };
-       readImage(resizedImage);                        
- next();
+       readImage(resizedImage);     */                    
+ 
 });
 
 
 
 
-
+export {
+   absoluteResizedImagePath,
+   absoluteImagePath,
+}
 export default retreiveImage;
